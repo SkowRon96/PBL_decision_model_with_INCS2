3d74a363f91e608da838e1968d70fba1494eb4af
Kamil Skowroński <donnpabloo@gmail.com>
2021-01-18 19:35:41 +0100

    Updated app.

--- INCS2_client.py
+++ INCS2_client.py
@@ -8,8 +8,7 @@ import numpy as np
 import logging as log
 import pandas as pd
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+
 
 def build_argparser():
     parser = ArgumentParser(add_help=False)
@@ -17,7 +16,7 @@ def build_argparser():
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
     args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model.", required=True,
                       type=str)
-    args.add_argument("-i", "--input", help="Required. Path to a folder with images or path to an image files",
+    args.add_argument("-i", "--input", help="Optional. Path to a folder with images or path to an image files",
                       type=str, nargs="+")
     args.add_argument("-l", "--cpu_extension",
                       help="Optional. Required for CPU custom layers. "
@@ -30,7 +29,8 @@ def build_argparser():
                       default="CPU", type=str)
     args.add_argument("--labels", help="Optional. Path to a labels mapping file", default=None, type=str)
     args.add_argument("-nt", "--number_top", help="Optional. Number of top results", default=10, type=int)
-
+    args.add_argument("-ip", "--ip_server", help="Optional. IP adress of server", default='127.0.0.1', type=str)
+    args.add_argument("-port", "--port_server", help="Optional. Port of server", default=65432, type=int)
     return parser
 
 def main():
@@ -54,7 +54,7 @@ def main():
     log.info("Preparing input blobs")
     input_blob = next(iter(net.input_info))
     out_blob = next(iter(net.outputs))
-    net.batch_size = len(args.input)
+    #net.batch_size = len(args.input)
 
     # Read and pre-process input inputs
     n, c = net.input_info[input_blob].input_data.shape
@@ -63,6 +63,8 @@ def main():
     log.info("Loading model to the plugin")
     exec_net = ie.load_network(network=net, device_name=args.device)
 
+    HOST = args.ip_server  # The server's hostname or IP address
+    PORT = args.port_server  # The port used by the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         while True:
             try:
